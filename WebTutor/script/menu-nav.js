4363e9ce311e72c8554380e03131001df61545f0
sadikovaa <sadikova.alsu01@gmail.com>
2022-12-11 21:56:52 +0300

    fix: fixing

--- WebTutor/script/menu-nav.js
+++ WebTutor/script/menu-nav.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', _ => {
 
     for (const child of nav.children) {
         if (child.pathname === document.location.pathname) {
-            child.style.background = '#FFFFFF';
-            child.style.color = '#B3AD4D';
+            child.className = 'selected-item'
             break;
         }
     }
